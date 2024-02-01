f3975574bbec0a455f5cba6760c6d913e04434c2
taifdroid <taifbakshi.dev@gmail.com>
2024-02-01 19:56:28 +0600

    removed buggy project

--- backend/src/files/dto/file-upload.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class FileUploadDTO implements Readonly<FileUploadDTO> {
-  @ApiProperty({ type: 'string', format: 'binary' })
-  file: Express.Multer.File;
-}
--- backend/src/files/dto/update-file.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { Folder } from '../../folders/entities/folder.entity';
-
-export class UpdateFileDto implements Readonly<UpdateFileDto> {
-    @ApiProperty()
-    folder: Folder;
-}
-
