15035e05fbe43e7d7241e239d92ccf40f6a2ef8d
taifdroid <taifbakshi.dev@gmail.com>
2024-02-02 21:05:16 +0600

    Solved new button materialize issue

--- frontend/app/components/createoptions/CreateOptionsComponent.tsx
+++ frontend/app/components/createoptions/CreateOptionsComponent.tsx
@@ -1,6 +1,7 @@
-import link from 'next/dist/client/link';
-import Head from 'next/head';
 import { useEffect, useState } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
 
 const CreateOptionsComponent = ({ showOptions, updateShowOptions }: any) => {
     useEffect(() => {
@@ -10,38 +11,20 @@ const CreateOptionsComponent = ({ showOptions, updateShowOptions }: any) => {
             M.Dropdown.init(dropdownTrigger, { coverTrigger: false });
         }
     }, []);
-    const [isHidden, setIsHidden] = useState(true); 
-    // console.log(isHidden);
-    // const updateHidden = () => {
-    //     console.log(11)
-    //     setIsHidden(!isHidden);        
-    // }
-    // console.log(isHidden);
-    // dropdown-trigger 
+
     return (
-        <>
-            <div className="nav-wrapper nav-2">
+        <div className="nav-wrapper nav-2">
             <a
-                className="btn waves-effect waves-light btn btn-flat white-text"              
-                // onClick={updateHidden}
+                className="dropdown-trigger btn waves-effect waves-light btn btn-flat white-text"
                 data-target="btn-dropdown"
             >New
             </a>
 
             <ul id='btn-dropdown' className='dropdown-content'>
-                {
-                    // isHidden === false ?(
-                        <>
-                            <li><a href="#!"><i className="material-icons">view_module</i>Create a folder</a></li>
-                            <li><a href="#!"><i className="material-icons">cloud</i>Upload a file</a></li>
-                        </>    
-                    // ) : null
-                }
-               
+                <li><a href="#!"><i className="material-icons">view_module</i>Create a folder</a></li>
+                <li><a href="#!"><i className="material-icons">cloud</i>Upload a file</a></li>
             </ul>
         </div>
-        </>
-        
     );
 }
 export default CreateOptionsComponent;
